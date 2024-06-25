c0b0936f546acd450ca83d0ffa144cb5c6627c50
wiseman-umanah <wisemanumanah@gmail.com>
2024-06-25 02:05:03 +0100

    This should pass the checks for 3

--- 0x00-pagination/3-hypermedia_del_pagination.py
+++ 0x00-pagination/3-hypermedia_del_pagination.py
@@ -35,24 +35,30 @@ class Server:
             dataset = self.dataset()
             truncated_dataset = dataset[:1000]
             self.__indexed_dataset = {
-                i: dataset[i] for i in range(len(dataset))
-            }
+                    i: dataset[i] for i in range(len(dataset))
+                    }
         return self.__indexed_dataset
 
     def get_hyper_index(self, index: int = None, page_size: int = 10) -> Dict:
+        """
+        deletion resilient hypermedia pagination
+        """
         dataset = self.indexed_dataset()
         assert type(index) is int and index in range(len(dataset))
+
         data = []
         start, end = index, index + page_size
+
         while start < end:
             if start in dataset.keys():
                 data.append(dataset[start])
             else:
                 end += 1
             start += 1
+
         return {
                 "index": index,
                 "data": data,
                 "page_size": len(data),
                 "next_index": end
-                }
+            }
