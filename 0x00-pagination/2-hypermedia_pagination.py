6b694959485d0deb7b6df66e1c601b5ba9459ed6
wiseman-umanah <wisemanumanah@gmail.com>
2024-06-25 02:03:27 +0100

    This should pass the checks

--- 0x00-pagination/2-hypermedia_pagination.py
+++ 0x00-pagination/2-hypermedia_pagination.py
@@ -46,22 +46,25 @@ class Server:
         """Function to get the data of a particular set"""
         assert type(page) is int and page > 0
         assert type(page_size) is int and page_size > 0
-        ran = index_range(page, page_size)
-        return self.dataset()[ran[0]: ran[1]]
+        indexes = index_range(page, page_size)
+        try:
+            data = self.dataset()
+            return data[indexes[0]: indexes[1]]
+        except IndexError:
+            return []
 
     def get_hyper(self, page: int = 1, page_size: int = 10) -> Dict:
         """Function to get the hyper version of data from source"""
-        assert type(page) is int and page > 0
-        assert type(page_size) is int and page_size > 0
-        data_size = len(self.dataset())
-        ran = index_range(page, page_size)
-        data = self.dataset()[ran[0]: ran[1]]
-        total = math.ceil(data_size / page_size)
+        data = self.get_page(page, page_size)
+        total_pages = math.ceil(len(self.dataset()) / page_size)
+        next_page = page + 1 if page < total_pages else None
+        prev_page = page - 1 if page > 1 else None
+
         return {
-            "page_size": len(data),
-            "page": page,
-            "data": data,
-            "next_page": page + 1 if page < total else None,
-            "prev_page": page - 1 if page > 1 else None,
-            "total_page": total
+                "page_size": len(data),
+                "page": page,
+                "data": data,
+                "next_page": next_page,
+                "prev_page": prev_page,
+                "total_pages": total_pages
         }
