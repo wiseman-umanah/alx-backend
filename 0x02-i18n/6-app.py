ce8f84a56fd404e38bcacd0759cdf0a33069ad47
wiseman-umanah <wisemanumanah@gmail.com>
2024-07-05 13:20:01 +0100

    Fixed up file

--- 0x02-i18n/6-app.py
+++ 0x02-i18n/6-app.py
@@ -3,7 +3,7 @@
 user’s preferred local if it is supported.
 """
 from flask import Flask, render_template, request, g
-from flask_babel import Babel, gettext as _
+from flask_babel import Babel
 
 
 users = {
@@ -14,37 +14,39 @@ users = {
 }
 
 
-app = Flask(__name__)
-app.config['BABEL_DEFAULT_LOCALE'] = 'en'
-app.config['BABEL_TRANSLATION_DIRECTORIES'] = 'translations'
-
-SUPPORTED_LOCALES = ['fr', 'en']
-
-
-def get_locale() -> str:
-    """Get locale for user
-
-    Returns:
-        str: The language format
+class Config(object):
     """
-    lang = request.args.get('locale')
-    if lang in SUPPORTED_LOCALES:
-        return lang
+    Configuration for Babel
+    """
+    LANGUAGES = ["en", "fr"]
+    BABEL_DEFAULT_LOCALE = "en"
+    BABEL_DEFAULT_TIMEZONE = "UTC"
+
+
+app = Flask(__name__)
+app.config.from_object(Config)
+babel = Babel(app)
+
+
+@babel.localeselector
+def get_locale():
+    """
+    Select and return best language match based on supported languages
+    """
     user = getattr(g, 'user', None)
-    if user and user['locale'] in SUPPORTED_LOCALES:
+    if user and user['locale'] in app.config['LANGUAGES']:
         return user['locale']
-
-    return request.accept_languages.best_match(SUPPORTED_LOCALES)
-
-
-babel = Babel(app, locale_selector=get_locale)
+    loc = request.args.get('locale')
+    if loc in app.config['LANGUAGES']:
+        return loc
+    return request.accept_languages.best_match(app.config['LANGUAGES'])
 
 
 def get_user():
     """Get user's id"""
-    user_id = request.args.get('login_as')
-    if user_id:
-        return users.get(int(user_id))
+    id = request.args.get('login_as', None)
+    if id is not None and int(id) in users.keys():
+        return users.get(int(id))
     return None
 
 
@@ -52,16 +54,14 @@ def get_user():
 def before_request():
     """Run before each request
     """
-    g.user = get_user()
+    user = get_user()
+    g.user = user
 
 
 @app.route('/')
 def index():
     """Index route"""
-    home_title = _('home_title')
-    home_header = _('home_header')
-    return render_template('5-index.html',
-                           home_title=home_title, home_header=home_header)
+    return render_template('6-index.html')
 
 
 if __name__ == '__main__':
