b959d9c85da8ec9b72893c4febc61452955703a4
wiseman-umanah <wisemanumanah@gmail.com>
2024-07-05 12:48:41 +0100

    Refactored code to use deprecated version v.2.0

--- 0x02-i18n/3-app.py
+++ 0x02-i18n/3-app.py
@@ -3,7 +3,7 @@
 with gettext
 """
 from flask import Flask, render_template, request
-from flask_babel import Babel, gettext as _
+from flask_babel import Babel
 
 app = Flask(__name__)
 
@@ -27,14 +27,10 @@ def get_locale():
     return request.accept_languages.best_match(app.config['LANGUAGES'])
 
 
-
 @app.route('/')
 def index():
     """index route"""
-    home_title = _('home_title')
-    home_header = _('home_header')
-    return render_template('3-index.html',
-                           home_title=home_title, home_header=home_header)
+    return render_template('3-index.html')
 
 
 if __name__ == '__main__':
